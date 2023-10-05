b70b1aa080e3effa7fbb58538f2a6ebe072f3705
Alvaro Mateos <alvaro.mateos@digitalvirgo.com>
2023-10-05 11:16:20 +0200

    nuevosEjercicios

--- practica_nueva/index.js
+++ practica_nueva/index.js
@@ -1,24 +1,54 @@
-let arrayEjemplo = [];
+let libro = {};
+let divBiblioteca = document.getElementById("biblioteca");
+let librosInicialesPantalla = false;
 
-function devolverEjemplo() {
-    let resultado = [];
-    arrayEjemplo = [63, 45, 58, 56];
-
-    arrayEjemplo.forEach(numero => {
-        resultado.push(numero + ',');
-    });
-
-    let textArea = document.getElementById("ejemplo");
-    return textArea.value = resultado;
-}
-
-function calcularOperaciones(){
-
-}
-function suma(array){
+function biblioteca(titulo, autor, año, genero) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.año = año;
+    this.genero = genero;
 
 }
 
+console.log(divBiblioteca);
+
+function obtenerDatosLibro() {
+    libro.titulo = prompt("nombre del libro");
+    libro.autor = prompt("nombre del auto");
+    libro.año = prompt("año cúando se escribío");
+    libro.genero = prompt("genero del libro");
+}
+
+function crearElementoParrafo() {
+}
+
+function mostrarInfoLibro() {
+    let parrafo = document.createElement("p");
+    obtenerDatosLibro();
+    parrafo.textContent = `Título : ${libro.titulo}, Autor: ${libro.autor}, Año: ${libro.año}, Género: ${libro.genero}`;
+    divBiblioteca.appendChild(parrafo);
+}
+
+function agregarLibro() {
+    mostrarInfoLibro();
+    biblioteca.push(libro);
+}
+
+function librosIniciales() {
+    if (!librosIniciales) {
+        new biblioteca['julio', 'perez', 1906, 'novela']
+        new biblioteca['julio', 'perez', 1906, 'novela']
+        new biblioteca['julio', 'perez', 1906, 'novela']
+
+        for (let i = 0; i < 3; i++) {
+            parrafo.textContent = "";
+        }
 
 
+    }
 
+}
+
+function eliminarLibro() {
+
+}
