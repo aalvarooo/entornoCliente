5c35fcbb6d1f7f06c31f01276a1c68dbaa9d8a2d
alvaro <alvaromar988@gmail.com>
2023-10-23 17:49:40 +0200

    realización del ejercicio 2-2

---
+++ tema2/practica_dom_2-2/script.js
@@ -0,0 +1,11 @@
+
+function mostrarGrande(imagen) {
+    const imagen_container= document.getElementById('imagen_container');
+    imagenNueva = imagen_container.lastElementChild;
+    console.log("lelgo aquí");
+    console.log(imagenNueva);
+    imagenNueva.src = `${imagen}`;
+    console.log(imagenNueva.src);
+    imagen_container.appendChild(imagenNueva);
+    
+}
