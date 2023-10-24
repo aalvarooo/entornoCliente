9f263c431c079e8a61615847290fcf75933bc798
alvaro <alvaromar988@gmail.com>
2023-10-24 17:49:54 +0200

    nuevos_ejercicios

--- tema2/practica_dom_2-3/script.js
+++ tema2/practica_dom_2-3/script.js
@@ -1,11 +1,43 @@
+const listaTareas = document.getElementById("lista_tareas");
 
-function mostrarGrande(imagen) {
-    const imagen_container= document.getElementById('imagen_container');
-    imagenNueva = imagen_container.lastElementChild;
-    console.log("lelgo aquí");
-    console.log(imagenNueva);
-    imagenNueva.src = `${imagen}`;
-    console.log(imagenNueva.src);
-    imagen_container.appendChild(imagenNueva);
-    
+const lista_li = listaTareas.children;
+
+for (let i = 0; i < lista_li.length; i++) {
+    lista_li[i].addEventListener(‘click’, function () {
+
+
+        lista_li[i].className = "resaltar";
+
+    });
 }
+
+function resaltarTarea() {
+
+
+}
+
+
+
+function agregarTarea() {
+    let textArea = document.getElementById("textarea").value;
+    const ultimo_li = listaTareas.lastChild;
+    const lista_nueva = document.createElement("li");
+    lista_nueva.textContent = textArea;
+    listaTareas.appendChild(lista_nueva);
+    textArea.textContent = "";
+}
+
+function eliminarTarea() {
+    let listaSelecionada = false;
+    for (let i = 0; i < lista_li.length; i++) {
+        if (lista_li[i].classList.contains('resaltado')) { // Hacer algo si la clase está presente } 
+            listaTareas.removeChild(lista_li[i]);
+        }
+    }
+}
+function moverTareaArriba() {
+
+}
+
+document.addEventListener("click", eliminarTarea);
+document.readyState
