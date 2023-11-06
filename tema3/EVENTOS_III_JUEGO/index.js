33cc8e9fa5915a20f79d1bb6c1ee6e233da28043
alvaro <alvaromar988@gmail.com>
2023-11-06 17:48:06 +0100

    Juego de barcos

--- tema3/EVENTOS_III_JUEGO/index.js
+++ tema3/EVENTOS_III_JUEGO/index.js
@@ -1,14 +1,81 @@
 const gameBoard = document.querySelector('.game-board');
-console.log(gameBoard);
 
-for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-        const gridCell = document.createElement("div");
-        gridCell.classList.add('grid');
-        gameBoard.appendChild(gridCell);
-    }
+
+
+for (let i = 0; i < 100; i++) {
+    const gridCell = document.createElement("div");
+    gridCell.classList.add('grid');
+    gameBoard.appendChild(gridCell);
+    gridCell.id = i + 1;
 }
+
+
 
 function crearBarcosAleatorios() {
 
+
 }
+
+function crearBarcosAleatorios() {
+    const arrayAleatorio = [];
+    for (let i = 0; i < 5; i++) {
+        let numAleatorio;
+        do {
+            numAleatorio = Math.floor(Math.random() * 100) + 1;
+        } while (arrayAleatorio.includes(numAleatorio)); // Evitar duplicados
+        arrayAleatorio.push(numAleatorio);
+    }
+    console.log(arrayAleatorio);
+    return arrayAleatorio;
+}
+
+
+function colocarBarcosEnTablero() {
+    const casillas = document.querySelectorAll('.grid');
+    const barcoPosiciones = crearBarcosAleatorios();
+
+    casillas.forEach((casilla, index) => {
+        if (barcoPosiciones.includes(index + 1)) {
+            casilla.classList.add('barco');
+        }
+    });
+}
+
+function hacerClick(e) {
+    const casilla = e.target;
+
+    if (casilla.classList.contains('barco')) {
+        alert("hundido");
+        casilla.classList.add('hundido');
+    } else {
+        casilla.classList.add('agua');
+        alert("agua");
+    }
+}
+
+
+const casillas = document.querySelectorAll('.grid');
+casillas.forEach(casilla => {
+    casilla.addEventListener("click", hacerClick);
+});
+
+colocarBarcosEnTablero();
+
+
+function gameOver() {
+
+}
+
+
+// TODO: hacer GameOver. 
+
+
+
+/* HACK:  
+1. Poner una imagen pequeña 
+2. Poner agua en la imagen
+3. que el barco ocupe 3 casillas
+4. Entonces el barco tiene 3 vidas
+5. 
+
+*/
